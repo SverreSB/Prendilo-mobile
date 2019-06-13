80cc33f3dc44d8ba2e3dc61c66dbb1f3e37c4674
SverreSB <sverre.sb@gmail.com>
2019-06-13 19:58:25 +0200

    added state

--- Prendilo/src/components/FoodList.js
+++ Prendilo/src/components/FoodList.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class FoodList extends Component {
+    state = { foods: [] };
     componentWillMount() {
-        console.log('Test')
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums') //using album api until I find better api or connect to backend
+            .then(response => this.setState({ foods: response.data}));
     }
 
     render() {
+        console.log(this.state);
         return (
             <View>
                 <Text>Food</Text>
