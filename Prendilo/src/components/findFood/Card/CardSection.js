61a0144cd37cd2b6dc7553dd8d1fba2531fcdf52
SverreSB <sverre.sb@gmail.com>
2019-06-18 13:07:00 +0200

    Fixed content for header within the card. This is temporary until I can find better API

--- Prendilo/src/components/findFood/Card/CardSection.js
+++ Prendilo/src/components/findFood/Card/CardSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import Card from './Card';
+
 
 const CardSection = ({children}) => {
     return(
--- Prendilo/src/components/findFood/FoodDetail.js
+++ Prendilo/src/components/findFood/FoodDetail.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, Image } from 'react-native'
 import Card from './Card/Card'
 import CardSection from './Card/CardSection'
+
 
 const FoodDetail = ({food}) => {
     return(
         <Card>
             <CardSection>
-                <CardImage/>
-                <CardText>
-                    {food.title}
-                    {food.name}
-                </CardText>
+                <Image
+                    style={style.imageStyle}
+                    source={{ uri: food.thumbnail_image }}
+                />
+                <View style={style.headerContentStyle}>
+                    <Text>{food.title}</Text>
+                    <Text>{food.artist}</Text>
+                </View>
             </CardSection>
         </Card>
     );
 };
+
+const style = {
+    headerContentStyle:  {
+        justifyContent: 'space-around',
+        flexDirection: 'column',
+    },
+    imageStyle: {
+        width: 50,
+        height: 50
+    }
+}
 
 export default FoodDetail
